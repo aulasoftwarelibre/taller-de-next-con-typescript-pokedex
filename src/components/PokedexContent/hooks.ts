6f12acf88be2e1abc72edf55f2dde7f9daa93ff7
Sergio Gómez Bachiller <sergio@uco.es>
2023-10-18 21:11:48 +0200

    fix: resolve bug than read the data twice

--- src/components/PokedexContent/hooks.ts
+++ src/components/PokedexContent/hooks.ts
@@ -1,13 +1,15 @@
 'use client'
 
-import { useSearchParams } from 'next/navigation'
 import { useEffect, useState } from 'react'
 
 import { PokedexContentProps } from './types'
 
 const useController = (props: PokedexContentProps) => {
   const [specimens, setSpecimens] = useState(props.specimens)
-  const searchParams = useSearchParams()
+
+  useEffect(() => {
+    setSpecimens(props.specimens)
+  }, [props.specimens])
 
   const onLike = async (id: number) => {
     await fetch(`/api/pokemon/${id}/like`, { method: 'POST' })
@@ -38,33 +40,6 @@ const useController = (props: PokedexContentProps) => {
       ),
     )
   }
-
-  useEffect(() => {
-    let ignore = false
-
-    async function updatePage() {
-      const limit = Number(searchParams.get('limit')) || 6
-      const offset = Number(searchParams.get('offset')) || 0
-
-      const result = await fetch(`/api/pokemon?limit=${limit}&offset=${offset}`)
-
-      if (!result.ok) {
-        throw new Error('Loading Error')
-      }
-
-      const { data } = await result.json()
-
-      if (!ignore) {
-        setSpecimens(data.specimens)
-      }
-    }
-
-    updatePage()
-
-    return () => {
-      ignore = true
-    }
-  }, [searchParams])
 
   return { onDislike, onLike, specimens }
 }
