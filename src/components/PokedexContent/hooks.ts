0bea18ed51f7079ffbca4a7fcab15a31f2575b2e
Sergio Gómez Bachiller <sergio@uco.es>
2023-10-19 18:53:21 +0200

    refactor: improve specimens load

--- src/components/PokedexContent/hooks.ts
+++ src/components/PokedexContent/hooks.ts
@@ -1,48 +1,21 @@
 'use client'
 
 import { useRouter } from 'next/navigation'
-import { useEffect, useState } from 'react'
 
 import { PokedexContentProps } from './types'
 
 const useController = (props: PokedexContentProps) => {
-  const [specimens, setSpecimens] = useState(props.specimens)
+  const { specimens } = props
   const router = useRouter()
-
-  useEffect(() => {
-    setSpecimens(props.specimens)
-  }, [props.specimens])
 
   const onLike = async (id: number) => {
     await fetch(`/api/pokemon/${id}/like`, { method: 'POST' })
-
-    setSpecimens(
-      specimens.map((specimen) =>
-        specimen.id === id
-          ? {
-              ...specimen,
-              liked: !specimen.liked,
-            }
-          : specimen,
-      ),
-    )
 
     router.refresh()
   }
 
   const onDislike = async (id: number) => {
     await fetch(`/api/pokemon/${id}/dislike`, { method: 'POST' })
-
-    setSpecimens(
-      specimens.map((specimen) =>
-        specimen.id === id
-          ? {
-              ...specimen,
-              liked: !specimen.liked,
-            }
-          : specimen,
-      ),
-    )
 
     router.refresh()
   }
