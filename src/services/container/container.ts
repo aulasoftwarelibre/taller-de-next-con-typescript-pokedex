b7ae642ba676964deb4ba97d89d469a2f1c15e49
Sergio Gómez Bachiller <sergio@uco.es>
2023-10-21 01:54:42 +0200

    refactor: remove in memory repository

--- src/services/container/container.ts
+++ src/services/container/container.ts
@@ -1,7 +1,6 @@
 import DislikePokemonUseCase from '@/lib/pokemon/application/use-cases/dislike-pokemon.use-case'
 import FindAllPokemonUseCase from '@/lib/pokemon/application/use-cases/find-all-pokemon.use-case'
 import LikePokemonUseCase from '@/lib/pokemon/application/use-cases/like-pokemon.use-case'
-import InMemoryPokemonRepository from '@/lib/pokemon/infrastructure/services/in-memory-pokemon-repository'
 import NextPokemonRepository from '@/lib/pokemon/infrastructure/services/next-pokemon-repository'
 
 import Database from '../database/database'
@@ -12,13 +11,8 @@ const pokeAPI = new PokeAPI()
 
 const { pokemonLikeTable } = new Database(config.database)
 
-const databases = {
-  inMemoryPokemonRepository: new InMemoryPokemonRepository(pokeAPI),
-  nextPokemonRepository: new NextPokemonRepository(pokeAPI, pokemonLikeTable),
-}
-
 // You can change here the database repository implementation
-const pokemonRepository = databases.nextPokemonRepository
+const pokemonRepository = new NextPokemonRepository(pokeAPI, pokemonLikeTable)
 
 const findAllPokemon = new FindAllPokemonUseCase(pokemonRepository)
 
