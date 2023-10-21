7b0631e99d7baf4f70223f0414c6ef20cb48291f
Sergio Gómez Bachiller <sergio@uco.es>
2023-10-21 12:58:10 +0200

    refactor: restructure code

--- src/components/PokedexContent/types.ts
+++
@@ -1,5 +0,0 @@
-import Pokemon from '@/lib/pokemon/domain/model/pokemon'
-
-export interface PokedexContentProps {
-  specimens: Pokemon[]
-}
--- src/components/PokemonCard/types.ts
+++ src/components/PokemonCard/types.ts
@@ -1,7 +1,5 @@
 import Pokemon from '@/lib/pokemon/domain/model/pokemon'
 
 export interface PokemonMiniCardProps {
-  onDisliked: (id: number) => void
-  onLiked: (id: number) => void
   pokemon: Pokemon
 }
