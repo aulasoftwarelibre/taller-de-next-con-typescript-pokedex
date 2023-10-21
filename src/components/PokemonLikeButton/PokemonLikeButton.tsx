046a8e45dd78e89cc786e172a45e08f65cf92f83
Sergio Gómez Bachiller <sergio@uco.es>
2023-10-21 12:56:00 +0200

    feat: add PokemonLikeButton

---
+++ src/components/PokemonLikeButton/PokemonLikeButton.tsx
@@ -0,0 +1,33 @@
+'use client'
+
+import { HeartIcon } from '@heroicons/react/24/outline'
+import { HeartIcon as FullHeartIcon } from '@heroicons/react/24/solid'
+import { Button } from '@nextui-org/react'
+
+import { useController } from '@/components/PokemonLikeButton/hooks'
+import { PokemonLikeButtonProps } from '@/components/PokemonLikeButton/types'
+
+export default function PokemonLikeButton(props: PokemonLikeButtonProps) {
+  const {
+    isLoading,
+    onDisliked,
+    onLiked,
+    pokemon: { liked },
+  } = useController(props)
+
+  return (
+    <Button
+      isDisabled={isLoading}
+      isIconOnly
+      radius="full"
+      variant="light"
+      onClick={() => (liked ? onDisliked() : onLiked())}
+    >
+      {liked ? (
+        <FullHeartIcon className="text-red-700" height="32" />
+      ) : (
+        <HeartIcon height="32" />
+      )}
+    </Button>
+  )
+}
